2605c9bfcc39ac614b52a972dcc6205c02a993e4
Diarmad Mac Ian <diarmad@diarmad.com>
2015-11-24 12:56:27 -0800

    initial commit.

---
+++ app/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.home'
+]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/home'});
+    }]);
