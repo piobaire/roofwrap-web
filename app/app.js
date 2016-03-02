0a4ce2c31846dfe312c265a55244cef95453ad8c
diarmad <alphanumeric_character@yahoo.com>
2016-03-01 19:55:23 -0800

    Added some placeholders for content

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 angular.module('roofwrapApp', [
   'ngMaterial',
   'ngRoute',
+  'ngAnimate',
   'ngSanitize',
   'roofwrapApp.base',
   'roofwrapApp.ScreenHelper',
@@ -9,7 +10,17 @@ angular.module('roofwrapApp', [
 .config(function ($routeProvider, $sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://roofwrap.com/dev/**']);
 
-  $routeProvider.when('/home', { templateUrl: 'templates/home.html', controller: 'BaseController'});
-  $routeProvider.when('/test', { templateUrl: 'templates/test.html', controller: 'BaseController'});
+  $routeProvider.when('/home',                  { templateUrl: 'templates/home.html', controller: 'BaseController'});
+  $routeProvider.when('/overview',              { templateUrl: 'templates/overview.html', controller: 'BaseController'});
+  $routeProvider.when('/product/descriptions',  { templateUrl: 'templates/descriptions.html', controller: 'BaseController' });
+  $routeProvider.when('/about',                 { templateUrl: 'templates/about.html', controller: 'BaseController' });
+  $routeProvider.when('/history',               { templateUrl: 'templates/history.html', controller: 'BaseController' });
+  $routeProvider.when('/brochure',              { templateUrl: 'templates/brochure.html', controller: 'BaseController' });
+  $routeProvider.when('/warranty',              { templateUrl: 'templates/warranty.html', controller: 'BaseController' });
+  $routeProvider.when('/faq',                   { templateUrl: 'templates/faq.html', controller: 'BaseController' });
+  $routeProvider.when('/contractor',            { templateUrl: 'templates/contractor.html', controller: 'BaseController' });
+  $routeProvider.when('/diagrams/construction', { templateUrl: 'templates/construction.html', controller: 'BaseController' });
+  $routeProvider.when('/diagrams/installation', { templateUrl: 'templates/installation.html', controller: 'BaseController' });
+  $routeProvider.when('/contact',               { templateUrl: 'templates/contact.html', controller: 'BaseController' });
   $routeProvider.otherwise({redirectTo: '/home'});
 });
